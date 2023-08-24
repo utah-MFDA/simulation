ec17318087caf97f2ee8f79645df466d15d9ec07
Brady Goenner <brady.goenner@utah.edu>
2023-08-23 19:09:45 -0600

    Can send to docker to run

--- runMFDASim.py
+++ runMFDASim.py
@@ -1,6 +1,12 @@
 
 import argparse
+import os
 
+import docker
+import tarfile
+
+# local imports
+from V2Va_Parser import Verilog2Xyce
 
 """
 Required inputs
@@ -19,13 +25,22 @@ Steps
 
 """
 
-def runSimulation(verilogFile, workDir):
+def runSimulation(
+        verilogFile, 
+        workDir, 
+        libraryFile,
+        cirConfigFile,
+        preRouteSim=False,
+        dockerContainer=None,
+        dockerWD=None):
     
+    simRunComm = ""
 
     # Convert to cir from v
-    convertToCir(verilogFile, workDir)
+    arcName = convertToCir(verilogFile, workDir, libraryFile, cirConfigFile, preRouteSim)
 
-    # transfer to docker image
+    # transfer files to docker image
+    pushCir2Docker(arcName, simRunComm, dockerContainer, dockerWD)
 
     # wait for simulator
 
@@ -35,24 +50,81 @@ def runSimulation(verilogFile, workDir):
 
     pass
 
-def convertToCir(verilogFile, wd):
+def convertToCir(verilogFile, wd, libFile, configFile, preRouteSim):
 
     # locate nessary files
-    specFile  = wd+verilogFile[:-2]+"_spec.csv"
-    lengthFile= wd+verilogFile[:-2]+"_length.xlsx"
-    devFile   = wd+"/devices.csv"
-    timeFile  = wd+"/simTime.csv" 
+    files = getSimFiles(verilogFile, wd)
+
+    Verilog2Xyce.Verilog2Xyce(
+        inputVerilogFile=files['verilogFile'],
+        configFile=configFile,
+        solnFile=files['specFile'],
+        remoteTestPath="",
+        libraryFile=libFile,
+        devFile=files["devFile"],
+        length_file=files["lengthFile"],
+        timeFile=files["timeFile"],
+        preRouteSim=preRouteSim,
+        outputVerilogFile=None,
+        runScipt=True)
+    
+    # create archive
+    arcNameBase = files['verilogFile'][:-2]+"_xyce"
+
+    xyceTar, arcName = createXyceArchive(arcNameBase)
+
+    srcDir = wd+"/spiceFiles"
+    xyceTar.add(srcDir, arcname=os.path.basename(srcDir.replace("spiceFiles",arcName.replace('.tar',''))))
+
+    xyceTar.close()
+
+    print("--------------------")
+    print("created archive: " + arcName)
+    print("--------------------")
+
+    return arcName
+
+def createXyceArchive(arcName, attempt=0):
+    newName = arcName+"_"+str(attempt)+".tar"
+    try:
+        xyceTar = tarfile.open(newName, 'x')
+        return xyceTar, newName
+    except FileExistsError:
+        return createXyceArchive(arcName, attempt=attempt+1)
 
 def getSimFiles(verilogFile, wd):
 
     files = {}
-    # locate nessary files
+    files['verilogFile']=wd+"/"+verilogFile
+    # locate necessary files
     files['specFile']  = wd+"/"+verilogFile[:-2]+"_spec.csv"
-    files['lengthFile']= wd+"/"+verilogFile[:-2]+"_length.xlsx"
+    files['lengthFile']= wd+"/"+verilogFile[:-2]+"_lengths.xlsx"
     files['devFile']   = wd+"/devices.csv"
     files['timeFile']  = wd+"/simTime.csv"
 
     return files
+
+def pushCir2Docker(simArchive, dockerContainer, dockerWD):
+
+    client = docker.from_env()
+
+    # check for running image
+    if dockerContainer not in [x.name for x in client.containers.list()]:
+        #print(client.containers.list())
+        raise ValueError('Container not in list (is it running?)' + "\n"+\
+                         "Running images: " + str([x.name for x in client.containers.list()]))
+    
+    # create archive
+    #tarfile.open()
+
+    xyceContainer = client.containers.get(dockerContainer)
+
+    with open(simArchive, 'rb') as fd:
+        ok = xyceContainer.put_archive(dockerWD, data=fd)
+        if not ok:
+            raise Exception('Put file failed')
+        else:
+            print("Files transfer success")
 
 if __name__ == "__main__":
     
--- testing/test_smart_t_script.py
+++ testing/test_smart_t_script.py
@@ -12,12 +12,58 @@ def test_file_load():
     files = runMFDASim.getSimFiles(verilogFile=vFile,
                             wd=wd)
 
+    verilogFile= wd+"/"+vFile
     specFile   = wd+"/smart_toilet_spec.csv"
-    lengthFile = wd+"/smart_toilet_length.xlsx"
+    lengthFile = wd+"/smart_toilet_lengths.xlsx"
     devFile    = wd+"/devices.csv"
     timeFile   = wd+"/simTime.csv"
 
-    assert files['specFile'] == specFile
-    assert files['lengthFile'] == lengthFile
-    assert files['devFile'] == devFile
-    assert files['timeFile']== timeFile
+    assert files['verilogFile'] == verilogFile
+    assert files['specFile']    == specFile
+    assert files['lengthFile']  == lengthFile
+    assert files['devFile']     == devFile
+    assert files['timeFile']    == timeFile
+
+def test_gen_cir():
+    import runMFDASim
+
+    wd = "./testing/smart_toilet_test"
+    vFile = "smart_toilet.v"
+    libFile="./testing/StandardCellLibrary.csv"
+    cirConfig = "./V2Va_Parser/VMF_xyce.mfsp"
+
+    runMFDASim.convertToCir(
+        verilogFile=vFile,
+        wd=wd,
+        libFile=libFile,
+        configFile=cirConfig,
+        preRouteSim=False)
+
+def test_push2Docker():
+
+    import runMFDASim
+
+    wd = "./testing/smart_toilet_test"
+    vFile = "smart_toilet.v"
+    libFile="./testing/StandardCellLibrary.csv"
+    cirConfig = "./V2Va_Parser/VMF_xyce.mfsp"
+
+    # Docker config
+    runSimComm = ""
+
+    dock_container = "vibrant_clarke"
+    dock_WD = "/mfda_simulation/local/simulations"
+
+    simTar = runMFDASim.convertToCir(
+        verilogFile=vFile,
+        wd=wd,
+        libFile=libFile,
+        configFile=cirConfig,
+        preRouteSim=False)
+
+    runMFDASim.pushCir2Docker(
+        simArchive=simTar, 
+        simRunComm=runSimComm, 
+        dockerContainer=dock_container, 
+        dockerWD=dock_WD)
+        
